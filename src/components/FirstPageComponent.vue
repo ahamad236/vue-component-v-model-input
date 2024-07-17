87a34a95f26559302673879c3da9bfd0ab7a20bd
ahamad236 <ahmadshaik4u@gmail.com>
2024-07-17 14:33:03 +0530

    first commit

---
+++ src/components/FirstPageComponent.vue
@@ -0,0 +1,32 @@
+<!-- <template>
+    <h2>
+        First Page Component
+    </h2>
+    <input v-model="model" />
+    <div>userName in Child is : {{ model }}</div>
+</template>
+
+<script setup lang="ts">
+    const model:any = defineModel()
+</script> -->
+
+
+<!-- with arguments from below -->
+<template>
+    <h2>
+        First Page Component
+    </h2>
+    <div>
+        <input v-model="firstName" />
+        <span>firstName in child  is : {{firstName}}</span>
+    </div>
+    <div>
+        <input v-model="lastName" />
+        <span>lastName in child is : {{ lastName }}</span>
+    </div>
+</template>
+<script setup lang="ts">
+    const firstName = defineModel('firstName')
+    const lastName = defineModel('lastName')
+</script>
+<!-- with arguments upto above -->
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,37 @@
+<!-- <template>
+    <h1>
+        Home Component
+    </h1>
+    <div>userName in Parent is : {{ userName }}</div>
+    <FirstPage v-model="userName"></FirstPage>
+</template>
+
+<script setup lang="ts">
+    import FirstPage from "./FirstPageComponent.vue"
+    import { ref } from "vue"
+    import type { Ref } from "vue"
+
+    const userName: Ref<string> = ref("Ahamad")
+</script> -->
+
+
+<!-- with arguments from below -->
+<template>
+    <h1> Home Component </h1>
+    <div>
+        FirstName is : {{ firstName }}
+    </div>
+    <div>
+        LastName is : {{ lastName }}
+    </div>
+    <FirstPage v-model:firstName="firstName" v-model:lastName="lastName"></FirstPage>
+</template>
+<script setup lang="ts">
+    import FirstPage from "./FirstPageComponent.vue"
+    import { ref } from 'vue'
+    import type { Ref } from "vue"
+
+    const firstName: Ref<string> = ref('Ahamad')
+    const lastName: Ref<string> = ref('Shaik')
+</script>
+<!-- with arguments upto above -->
